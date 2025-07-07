9b4d80463fe6567a1e6106a4546e968e368ee3e8
subixsh04 <subikshasvaidhyanathan@gmail.com>
2025-07-07 22:38:45 +0530

    started homepage

--- event-platform/src/app/app.component.ts
+++ event-platform/src/app/app.component.ts
@@ -1,12 +1,14 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
-  template: `<router-outlet></router-outlet>`,
+  imports: [RouterModule, NavbarComponent],
+  template: `<app-navbar></app-navbar> 
+              <router-outlet></router-outlet>`,
   styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {}
