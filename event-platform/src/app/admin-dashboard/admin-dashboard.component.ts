101d566f9938e1e104b7194b0e137a0fbb7beed3
subixsh04 <subikshasvaidhyanathan@gmail.com>
2025-07-14 23:03:11 +0530

    started working on admin componenets like add, edit and delete

--- event-platform/src/app/admin-dashboard/admin-dashboard.component.ts
+++ event-platform/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-admin-dashboard',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './admin-dashboard.component.html',
-  styleUrl: './admin-dashboard.component.css'
+  styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent {
+
+  showAddForm = false;
+
+  newEvent = {title: '', date: '', description: '', image: '' };
+
+  events = [{ title: 'Sample Event', date: '2025-08-01', description: 'This is a sample event.', image: '' }];
+
+  addEvent() {
+    this.events.push({ ...this.newEvent });
+    this.newEvent = { title: '', date: '', description: '', image: '' };
+    this.showAddForm = false;
+  }
+
+  deleteEvent(index: number) {
+    this.events.splice(index, 1);
+  }
+
+  editEvent(index: number) {
+    const event = this.events[index];
+    this.newEvent = { ...event };
+    this.deleteEvent(index);
+    this.showAddForm = true;
+  }
 
 }
